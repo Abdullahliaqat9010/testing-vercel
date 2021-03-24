788b14ead20c5414c84cf893ea479c1fcd79c26e
SergeyZhilin <szhilin87@gmail.com>
2021-03-24 11:45:28 +0200

    feat: add design estimate block section

--- src/pages/Dashboard/EstimateBlock/index.tsx
+++ src/pages/Dashboard/EstimateBlock/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, ButtonGroup, OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 const EstimateBlock = () => {
   return (
@@ -7,13 +8,32 @@ const EstimateBlock = () => {
       <p>Route des Cent Ecus, 24370 Sainte Mondane</p>
       <div className="scale-block">
         <div className="line">
-          <div className="circle">
-
-          </div>
+          <OverlayTrigger
+            key='tooltip'
+            placement='top'
+            show
+            overlay={
+              <Tooltip id='price-block'>
+                €1,097,500
+              </Tooltip>
+            }
+          >
+            <div className="circle"/>
+          </OverlayTrigger>
         </div>
         <div className="range d-flex justify-content-between">
           <div className="min">€ <span>1,009,750</span></div>
           <div className="max">€ <span>1,185,250</span></div>
+        </div>
+        <div className="btn-block">
+          <span className='btn-block__title w-100'>How accurate is this estimation?</span>
+          <ButtonGroup size="lg" className="w-100">
+            <Button>Inaccurate</Button>
+            <Button>Low</Button>
+            <Button>Medium</Button>
+            <Button>High</Button>
+            <Button>Exact</Button>
+          </ButtonGroup>
         </div>
       </div>
     </div>
