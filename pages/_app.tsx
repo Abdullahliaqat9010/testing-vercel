67c6c4b1bb8ee1a97f29a049b2119a88579e4fd1
SergeyZhilin <szhilin87@gmail.com>
2021-04-14 16:08:03 +0300

    feat: add google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
+import { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from '../store';
+
+import TagManager from 'react-gtm-module';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -17,7 +20,16 @@ import '../styles/pages/dashboard.scss';
 import '../styles/pages/agency.scss';
 import '../styles/pages/login.scss';
 
+const tagManagerArgs = {
+  gtmId: 'GTM-KGQ67XP'
+}
+
 const MyApp = ({ Component, pageProps }) => {
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
+
   return(
     <Provider store={ store }>
       <PersistGate loading={ null } persistor={ persistor }>
