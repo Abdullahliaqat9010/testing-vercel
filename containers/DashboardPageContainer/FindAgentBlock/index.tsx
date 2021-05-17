620499b31ae61cb0a9457cb24877a83d08d4f3b2
SergeyZhilin <szhilin87@gmail.com>
2021-05-17 17:03:21 +0300

    feat: makes button load more working

--- containers/DashboardPageContainer/FindAgentBlock/index.tsx
+++ containers/DashboardPageContainer/FindAgentBlock/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'next-i18next';
 
-// import { Button } from 'react-bootstrap';
-// import LoadMoreImage from '../../../assets/images/load-more.svg';
+import { Button } from 'react-bootstrap';
+import LoadMoreImage from '../../../assets/images/load-more.svg';
 
 import Agency from '../../../containers/Agency';
 
@@ -10,19 +10,31 @@ import { agentsList } from '../../../templates/agentsList';
 
 const FindAgentBlock = () => {
   const {t} = useTranslation('dashboard-page');
+
+  const elementsOnPage = 3;
+  const [sizeArr, setSizeArr] = useState(elementsOnPage);
+  const agencyList = agentsList.slice(0, sizeArr);
+
+  const loadMore = () => {
+    setSizeArr(sizeArr + elementsOnPage);
+  }
+
   return (
     <div className='find-agent-block'>
       <h3>{t('title.find-your-agent')}</h3>
       <p>{t('desc.we-found')} { agentsList.length } {t('desc.agents-near-you')}</p>
       {
-        agentsList.map(
+        agencyList.map(
           (agency, index) =>
             <Agency agency={agency} key={index}/>
         )
       }
-      {/*<Button className='load-more'>*/}
-      {/*  <img src={ LoadMoreImage } alt="LoadMoreImage"/>{ t('button.load-more') }*/}
-      {/*</Button>*/}
+      {
+        agencyList.length < agentsList.length &&
+        <Button className='load-more' onClick={loadMore}>
+          <img src={ LoadMoreImage } alt="LoadMoreImage"/>{ t('button.load-more') }
+        </Button>
+      }
     </div>
   )
 }
