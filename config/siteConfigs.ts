57a1c6ccd3898b1ce007cebb49fb9252e1d77815
Abdullah <21100270@lums.edu.pk>
2021-07-16 00:43:02 +0500

    formatting

--- config/siteConfigs.ts
+++ config/siteConfigs.ts
@@ -1,14 +1,15 @@
 // Site Config =============================================================
 export const config = {
-  siteTitle: process.env.SITE_TITLE,
-  apiDomain: process.env.APP_API,
-  metricKey: process.env.GOOGLE_METRIC_KEY,
-  cookieKey: process.env.COOKIE_KEY,
+	siteTitle: process.env.SITE_TITLE,
+	apiDomain: process.env.API,
+	metricKey: process.env.GOOGLE_METRIC_KEY,
+	cookieKey: process.env.COOKIE_KEY,
 };
 
 // Google Maps Configs =====================================================
 export const googleMapConfig = {
-  apiKey: process.env.GOOGLE_MAP_API_KEY || '',
+	apiKey: process.env.GOOGLE_MAP_API_KEY || "",
 };
 
-export const userToken = typeof window === 'object' && localStorage.getItem('auth');
+export const userToken =
+	typeof window === "object" && localStorage.getItem("auth");
