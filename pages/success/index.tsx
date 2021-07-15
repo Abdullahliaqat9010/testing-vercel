e0d97b9b7f0c65775f1571912a79281d73299da8
SergeyZhilin <szhilin87@gmail.com>
2021-07-15 14:28:05 +0300

    feat: Tracking accurately, click "I alread have an account"

---
+++ pages/success/index.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import DashboardPageContainer from '../../containers/DashboardPageContainer';
+
+import { userToken } from '../../config/siteConfigs';
+
+const SuccessPage = () => {
+  const router = useRouter();
+  const {locale} = router;
+
+  useEffect(() => {
+    if (userToken) {
+      router.push('/dashboard', locale + '/dashboard', {locale: locale});
+    }
+  }, []);
+
+  return (
+    <DashboardPageContainer/>
+  );
+};
+
+export const getStaticProps = async ({locale}) => ({
+  props: {
+    ...await serverSideTranslations(locale, ['dashboard-page', 'header', 'common']),
+  },
+});
+
+export default SuccessPage;
