b0435495fcbec23e08bcb6dd7b31c4a2068d1183
Abdullah <21100270@lums.edu.pk>
2021-08-12 03:58:14 +0500

    fixes on blogs

--- components/3dMap/components/map.tsx
+++ components/3dMap/components/map.tsx
@@ -5,35 +5,30 @@ import { useEffect } from "react";
 
 const Map = () => {
 	useEffect(() => {
-		var map = new OSMBuildings({
-			container: "map",
-			position: { latitude: 52.51836, longitude: 13.40438 },
-			zoom: 16,
-			minZoom: 15,
-			maxZoom: 20,
-			attribution:
-				'© Data <a href="https://openstreetmap.org/copyright/">OpenStreetMap</a> © Map <a href="https://mapbox.com/">Mapbox</a> © 3D <a href="https://osmbuildings.org/copyright/">OSM Buildings</a>',
-		});
-		map.addMapTiles("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
-		map.addGeoJSONTiles(
-			"https://{s}.data.osmbuildings.org/0.2/anonymous/tile/{z}/{x}/{y}.json"
-		);
-		map.setTilt(45);
+		// var map = new OSMBuildings({
+		// 	container: "map",
+		// 	position: { latitude: 52.51836, longitude: 13.40438 },
+		// 	zoom: 16,
+		// 	minZoom: 15,
+		// 	maxZoom: 20,
+		// 	attribution:
+		// 		'© Data <a href="https://openstreetmap.org/copyright/">OpenStreetMap</a> © Map <a href="https://mapbox.com/">Mapbox</a> © 3D <a href="https://osmbuildings.org/copyright/">OSM Buildings</a>',
+		// });
+		// map.addMapTiles("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+		// map.addGeoJSONTiles(
+		// 	"https://{s}.data.osmbuildings.org/0.2/anonymous/tile/{z}/{x}/{y}.json"
+		// );
+		// map.setTilt(45);
 		// var map = new L.Map("map");
-
 		// map.setView([52.51836, 13.40438], 16, false);
-
 		// new L.TileLayer(
 		// 	"https://{s}.tiles.mapbox.com/v3/[YOUR_MAPBOX_KEY]/{z}/{x}/{y}.png",
 		// 	{
 		// 		attribution: '© Map tiles <a href="https://mapbox.com">Mapbox</a>',
-
 		// 		maxZoom: 18,
-
 		// 		maxNativeZoom: 20,
 		// 	}
 		// ).addTo(map);
-
 		// var osmb = new OSMBuildings(map).load(
 		// 	"https://{s}.data.osmbuildings.org/0.2/anonymous/tile/{z}/{x}/{y}.json"
 		// );
