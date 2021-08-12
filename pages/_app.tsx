055cadcadb403f86e3987480669f4d5dc33679eb
cs <abdullah.liaqat9010@gmail.com>
2021-08-13 01:01:20 +0500

    diabled google translate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { PersistGate } from "redux-persist/integration/react";
 import { appWithTranslation, useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import Head from "next/head";
-import { Html } from "next/document";
 import axios from "axios";
 
 import { store, persistor } from "../store";
@@ -67,42 +66,11 @@ const MyApp = ({ Component, pageProps }) => {
 
 	return (
 		<>
-			{/* <Head>
-				<link
-					href="https://cdn.osmbuildings.org/4.1.1/OSMBuildings.css"
-					rel="stylesheet"
-				/>
-
-				<script src="https://cdn.osmbuildings.org/4.1.1/OSMBuildings.js" />
-			</Head> */}
-			{/* <body> */}
 			<Provider store={store}>
 				<PersistGate loading={null} persistor={persistor}>
-					{/* <Html /> */}
-					<Head>
-						<link
-							href="https://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css"
-							rel="stylesheet"
-						/>
-						<link
-							href="https://cdn.osmbuildings.org/4.1.1/OSMBuildings.css"
-							rel="stylesheet"
-						/>
-						<link rel="preconnect" href="https://fonts.googleapis.com" />
-						<link rel="preconnect" href="https://fonts.gstatic.com" />
-						<link
-							href="https://fonts.googleapis.com/css2?family=Cormorant:wght@400;500;600&family=Nunito+Sans:wght@400;700;800&display=swap"
-							rel="stylesheet"
-						/>
-						<script src="https://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js" />
-						<script src="https://cdn.osmbuildings.org/classic/0.2.2b/OSMBuildings-Leaflet.js" />
-						<script src="https://cdn.osmbuildings.org/4.1.1/OSMBuildings.js" />
-					</Head>
 					<Component {...pageProps} />
-					{/* </Html> */}
 				</PersistGate>
 			</Provider>
-			{/* </body> */}
 		</>
 	);
 };
