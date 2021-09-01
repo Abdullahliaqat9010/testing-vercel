5a88ef423c5dbd257812357db4fd11d1cb160d6c
Ashrafali1122 <ashrafali.gcs1122@gmail.com>
2021-09-01 19:27:46 +0500

    apply validations on reset password

--- pages/reset-password/index.tsx
+++ pages/reset-password/index.tsx
@@ -14,20 +14,20 @@ import BackArrow from '../../assets/images/full-arrow.svg';
 
 import { closeChangePasswordModalAction, sendDataForUpdatePasswordAction } from '../../actions';
 import { RootState } from '../../types/state';
-
+import { regexp } from '../../utils';
 const ResetPasswordPage = () => {
-  const {t} = useTranslation('login-page');
+  const { t } = useTranslation('login-page');
 
   const dispatch = useDispatch();
   const router = useRouter();
-  const {locale} = router;
-  const {token} = router.query;
-  const { changePasswordModal, error  } = useSelector((state: RootState) => state.modals);
-  const [userData, setUserData] = useState({password: '', confirmPassword: ''});
+  const { locale } = router;
+  const { token } = router.query;
+  const { changePasswordModal, error } = useSelector((state: RootState) => state.modals);
+  const [userData, setUserData] = useState({ password: '', confirmPassword: '' });
   const [errorsData, setErrorsData] = useState({
     noValid: false,
-    password: 'this field is required',
-    confirmPassword: 'this field is required',
+    password: '',
+    confirmPassword: '',
   });
 
   const handleChangeData = (el: React.ChangeEvent<HTMLInputElement>) => {
@@ -35,6 +35,11 @@ const ResetPasswordPage = () => {
       ...userData,
       [el.target.name]: el.target.value.trim(),
     });
+    setErrorsData({
+      noValid: false,
+      password: '',
+      confirmPassword: '',
+    })
   };
 
   const closeModal = () => {
@@ -45,28 +50,13 @@ const ResetPasswordPage = () => {
   const handleSendData = (event) => {
     event.preventDefault();
     if (userData.password.length === 0 || userData.confirmPassword.length === 0) {
+      console.log("userData", userData)
       return setErrorsData({
         noValid: true,
         password: 'this field is required',
         confirmPassword: 'this field is required',
       });
-    }
-
-    if (userData.password.length < 5 || userData.confirmPassword.length < 5) {
-      return setErrorsData({
-        noValid: true,
-        password: userData.password.length < 5 ? 'password should be more then 5 symbols' : '',
-        confirmPassword: userData.confirmPassword.length < 5 ? 'password should be more then 5 symbols' : '',
-      });
-    }
-
-    if (userData.password !== userData.confirmPassword) {
-      return setErrorsData({
-        ...errorsData,
-        noValid: true,
-        confirmPassword: 'Passwords dont match',
-      });
-    } else {
+    }else {
       setErrorsData({
         noValid: false,
         confirmPassword: '',
@@ -80,9 +70,46 @@ const ResetPasswordPage = () => {
     }
   };
 
+  const paswordValidate = (value) => {
+    console.log("value", value.match(regexp.password))
+    if (value && !value.match(regexp.password)) {
+      setErrorsData({
+        ...errorsData,
+        noValid: false,
+        password: 'Password should contain at least one lower and Upper letter and number',
+      });
+    }
+  }
+  const confirmPaswordValidate = (value) => {
+    if (value && !value.match(regexp.password)) {
+      setErrorsData({
+        ...errorsData,
+        noValid: false,
+        confirmPassword: 'Password should contain at least one lower and Upper letter and number',
+      });
+
+    }
+    if (userData.password.length < 5 || userData.confirmPassword.length < 5) {
+       setErrorsData({
+        ...errorsData,
+        noValid: false,
+        confirmPassword: userData.confirmPassword.length < 5 ? 'password should be more then 5 symbols' : '',
+      });
+
+    }
+    if (userData.password !== userData.confirmPassword) {
+       setErrorsData({
+        ...errorsData,
+        noValid: false,
+        confirmPassword: 'Passwords dont match',
+      });
+    }
+  }
+
+
   return (
     <div className='ResetPassword'>
-      <Alert className='custom-alert' show={changePasswordModal} variant={ error.length ? 'danger' : 'success' }>
+      <Alert className='custom-alert' show={changePasswordModal} variant={error.length ? 'danger' : 'success'}>
         <Alert.Heading>
           {
             error.length ? 'Oh snap! You got an error!' : "Congratulations!"
@@ -95,74 +122,80 @@ const ResetPasswordPage = () => {
         </p>
         <hr />
         <div className="d-flex justify-content-end">
-          <Button onClick={closeModal} variant={ error.length ? 'outline-danger' : 'outline-success' }>
+          <Button onClick={closeModal} variant={error.length ? 'outline-danger' : 'outline-success'}>
             Close me!
           </Button>
         </div>
       </Alert>
-      <HeaderContainer title="Reset your password"/>
+      <HeaderContainer title="Reset your password" />
       <div className="bg-image">
         <h1 className='h4'>Reset password</h1>
       </div>
       <p className='desc'>
         If you know your password please
-        <Link href={ '/login' } locale={ locale }>
-          <span className='link'> go back to login <img src={ ArrowLink } alt="ArrowLink"/></span>
+        <Link href={'/login'} locale={locale}>
+          <span className='link'> go back to login <img src={ArrowLink} alt="ArrowLink" /></span>
         </Link>
       </p>
-      <Form validated={ errorsData.noValid }>
+      <Form validated={errorsData.noValid}>
         <Form.Group controlId="password">
           <Form.Label>Password</Form.Label>
-          <img src={ LockIcon } alt="LockIcon"/>
+          <img src={LockIcon} alt="LockIcon" />
           <Form.Control
             name="password"
             required
-            minLength={ 5 }
+            minLength={5}
             type="password"
-            onChange={ handleChangeData }
+            onBlur={(el)=> paswordValidate(el.target.value)}
+            onChange={handleChangeData}
+            isInvalid={errorsData.password.length> 0}
           />
           <Form.Control.Feedback type="invalid">
-            { errorsData.password }
+            {errorsData.password}
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group controlId="confirm-password">
           <Form.Label>Confirm Password</Form.Label>
-          <img src={ LockIcon } alt="LockIcon"/>
+          <img src={LockIcon} alt="LockIcon" />
           <Form.Control
             name="confirmPassword"
             required
-            minLength={ 5 }
-            isInvalid={ errorsData.confirmPassword === 'Passwords dont match' }
+            minLength={5}
+            // isInvalid={errorsData.confirmPassword === 'Passwords dont match'}
             type="password"
-            onChange={ handleChangeData }
+            onChange={handleChangeData}
+            onBlur={(el)=> confirmPaswordValidate(el.target.value)}
+
+            isInvalid={errorsData.confirmPassword.length> 0}
+
           />
           <Form.Control.Feedback type="invalid">
-            { errorsData.confirmPassword }
+            {errorsData.confirmPassword}
           </Form.Control.Feedback>
         </Form.Group>
         <div className="group-btn">
-          <Button type='submit' className='send-reminder' onClick={ handleSendData }>
+          <Button type='submit' className='send-reminder' onClick={handleSendData}>
             Change Password
           </Button>
-          <Link href='/' locale={ locale }>
-            <span><img src={ BackArrow } alt="BackArrow"/>{ t('button.back') }</span>
+          <Link href='/' locale={locale}>
+            <span><img src={BackArrow} alt="BackArrow" />{t('button.back')}</span>
           </Link>
         </div>
       </Form>
       <div className="short-footer d-flex justify-content-between">
         <p>
           <span>Immo Belgium </span>
-          <span>{ new Date().getFullYear() }. All Rights Reserved.</span>
+          <span>{new Date().getFullYear()}. All Rights Reserved.</span>
         </p>
         <span className="link">
-          <a href={ '/' + locale + '/privacy-policy' } target='_blank'>Politique de Confidentialité.</a>
+          <a href={'/' + locale + '/privacy-policy'} target='_blank'>Politique de Confidentialité.</a>
         </span>
       </div>
     </div>
   );
 };
 
-export const getStaticProps = async ({locale}) => ({
+export const getStaticProps = async ({ locale }) => ({
   props: {
     ...await serverSideTranslations(locale, ['login-page', 'header']),
   },
