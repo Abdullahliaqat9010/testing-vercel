3a3ad4a5892db0ba8331da337911fe7a22d7629a
Ashrafali1122 <ashrafali.gcs1122@gmail.com>
2021-10-01 15:50:28 +0500

    price coloring settings and pending some work

--- components/MapboxPriceMap/index.tsx
+++ components/MapboxPriceMap/index.tsx
@@ -48,6 +48,8 @@ const Mapbox3dMap = ({
     ]);
 
     // const mapRef = useRef(null);
+    console.log("features", province.features.length)
+
 
     useEffect(() => {
         mapboxgl.accessToken = 'pk.eyJ1IjoiYXNocmFmYWxpMTEyMiIsImEiOiJja3Rkd2UzaHUyazg3MnVwZ2w4YjFubTh3In0.XU0TSvROhCasiUBhLaCbiQ';
@@ -57,6 +59,20 @@ const Mapbox3dMap = ({
             center: [4.402771, 51.260197], // starting position
             zoom: 8 // starting zoom
         });
+        const colorsArra = ['#8F99B4', "#FE7F2D", "#3871EF", "#1D2E5B", '#8F99B4', "#FE7F2D", "#3871EF", "#1D2E5B", "#FE7F2D", "#1D2E5B"]
+
+        const matchExpression = [];
+
+        // Calculate color values for each country based on 'hdi' value
+        for (const row of province.features) {
+            // Convert the range of data values to a suitable color
+            // let theRandomNumber = Math.floor(Math.random() * 4)
+            let color = colorsArra[row.price]
+            matchExpression.push([row["price"], color]);
+        }
+        // matchExpression.push('rgb(112 26 26)')
+        console.log("matchExpression", matchExpression)
+
 
         map.on('load', () => {
             // Add a data source containing GeoJSON data.
@@ -72,10 +88,43 @@ const Mapbox3dMap = ({
                 'source': 'provice_geojson', // reference the data source
                 'layout': {},
                 'paint': {
-                    'fill-color': '#0080ff', // blue color fill
-                    'fill-opacity': 0.5
+                    'fill-color': {
+                        property: 'price',
+                        stops: matchExpression
+                    } // blue color fill
+                    
                 }
-            });
+            },'waterway-label',);
+
+            // function setStates() {
+            //     for (let index = 0; index < province.features.length; index++) {
+            //         const { NAME_4 } = province.features[index];
+            //         map.setFeatureState(
+            //             {
+            //                 id: index
+            //             },
+            //             {
+            //                 NAME_4: NAME_4
+            //             }
+            //         );
+
+            //     }
+            // }
+
+            // // Check if `statesData` source is loaded.
+            // function setAfterLoad({ sourceID, isSourceLoaded }) {
+            //     if (sourceID !== 'provice_geojson' && !isSourceLoaded) return;
+            //     setStates();
+            //     map.off('sourcedata', setAfterLoad);
+            // }
+
+            // // If `statesData` source is loaded, call `setStates()`.
+            // if (map.isSourceLoaded('provice_geojson')) {
+            //     setStates();
+            // } else {
+            //     map.on('sourcedata', setAfterLoad);
+            // }
+
             // Add a black outline around the polygon.
             map.addLayer({
                 'id': 'outline',
@@ -105,7 +154,7 @@ const Mapbox3dMap = ({
 
 
     return (
-        <div style={{ height: "100vh", width: "100%" }}  id="map" />
+        <div style={{ height: "100vh", width: "100%" }} id="map" />
     );
 };
 
--- next-env.d.ts
+++ next-env.d.ts
@@ -7,4 +7,4 @@ declare module "*.jpeg";
 declare module "*.jpg";
 declare module "*.svg";
 declare module "*.ttf";
-declare module "*.geojson";
+declare module "*.kml";
