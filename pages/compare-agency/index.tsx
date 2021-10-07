40f2110274bad4998e41beb49d4516c608649072
Zarish173 <zarishsajid007@gmail.com>
2021-10-07 21:07:39 +0500

    half issues in trello testing failed card resolved

--- pages/compare-agency/index.tsx
+++ pages/compare-agency/index.tsx
@@ -1,42 +1,51 @@
-import React, { useState , useEffect} from "react"
+import React, { useState, useEffect } from "react";
 
-import { serverSideTranslations } from "next-i18next/serverSideTranslations"
-import HeaderContainer from "../../containers/Header"
-import FooterContainer from "../../containers/Footer"
-import Link from "next/link"
-import { Button, InputGroup, FormControl, Carousel, ListGroup } from "react-bootstrap"
-import goAhead from "../../assets/images/compare-agency/go-ahead.svg"
-import reviewImage from "../../assets/images/compare-agency/reviews-image.png"
-import locationImage from "../../assets/images/compare-agency/location-image.png"
-import homeImage from "../../assets/images/compare-agency/home-image.png"
-import compareAgencyImage from "../../assets/images/compare-agency/agency-main-page-image.jpg"
-import StarRatingComponent from "react-star-rating-component"
-import RatingStar from '../../assets/images/rating/full-star.svg';
-import RatingStarEmpty from '../../assets/images/rating/star.svg';
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import HeaderContainer from "../../containers/Header";
+import FooterContainer from "../../containers/Footer";
+import Link from "next/link";
+import {
+  Button,
+  InputGroup,
+  FormControl,
+  Carousel,
+  ListGroup,
+} from "react-bootstrap";
+import goAhead from "../../assets/images/compare-agency/go-ahead.svg";
+import reviewImage from "../../assets/images/compare-agency/reviews-image.png";
+import locationImage from "../../assets/images/compare-agency/location-image.png";
+import homeImage from "../../assets/images/compare-agency/home-image.png";
+import compareAgencyImage from "../../assets/images/compare-agency/agency-main-page-image.jpg";
+import StarRatingComponent from "react-star-rating-component";
+import RatingStar from "../../assets/images/rating/full-star.svg";
+import RatingStarEmpty from "../../assets/images/rating/star.svg";
 import HomeownerIcon from "../../assets/images/home-noactive.svg";
-import { useRouter } from "next/router"
-import { useDispatch, useSelector } from "react-redux"
-import { getAutocompleteItemsAction, clearAutocompleteItems } from "../../actions"
-import { RootState } from "../../types/state"
+import { useRouter } from "next/router";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getAutocompleteItemsAction,
+  clearAutocompleteItems,
+} from "../../actions";
+import { RootState } from "../../types/state";
 
 const compareAgency = () => {
   const router = useRouter();
 
-  const dispatch = useDispatch()
-  const { locale } = router
+  const dispatch = useDispatch();
+  const { locale } = router;
   const [value, setValue] = useState("");
   const [dataInfo, setData] = useState({});
   const { dataFromMapBox } = useSelector((state: RootState) => state.stepsInfo);
 
-  useEffect(()=> {
+  useEffect(() => {
     dispatch(clearAutocompleteItems());
-  },[])
+  }, []);
 
   const gotoAgenciesPages = () => {
     router.push({
-      pathname: locale + "/agency-result"
-    })
-  }
+      pathname: locale + "/agency-result",
+    });
+  };
   const handleAutocomplete = (el: React.ChangeEvent<HTMLInputElement>) => {
     setValue(el.target.value);
     if (el.target.value.length > 0) {
@@ -63,13 +72,11 @@ const compareAgency = () => {
       country: currentAddress.country,
     };
 
-    localStorage.setItem("address", JSON.stringify(dataForNextStep))
+    localStorage.setItem("address", JSON.stringify(dataForNextStep));
 
     setData({ ...dataForNextStep });
     dispatch(clearAutocompleteItems());
   };
-
-
 
   return (
     <>
@@ -102,9 +109,18 @@ const compareAgency = () => {
           </div>
         </div> */}
         <div className="search-area">
-          <p> <span> Compare Real Estate <br></br>Agencies in your neighbourhood.</span></p>
-          <p>We analyze thousands of local agents and find <br></br>the best to compete you!</p>
-          <div className="search-form d-flex" >
+          <p>
+            {" "}
+            <span>
+              {" "}
+              Compare Real Estate <br></br>Agencies in your neighbourhood.
+            </span>
+          </p>
+          <p>
+            We analyze thousands of local agents and find <br></br>the best to
+            compete you!
+          </p>
+          <div className="search-form d-flex">
             <div className="d-flex flex-collumn">
               <InputGroup>
                 <FormControl
@@ -116,9 +132,23 @@ const compareAgency = () => {
                 />
               </InputGroup>
               {dataFromMapBox.length > 0 && (
-                <ListGroup as="ul" className="position-absolute" style={{ marginTop: "45px", width: "288px" }}>
+                <ListGroup
+                  as="ul"
+                  className="position-absolute"
+                  style={{ marginTop: "45px", width: "288px" }}
+                >
                   {dataFromMapBox.map((item, index) => (
-                    <ListGroup.Item className='text-dark' as="li" onClick={() => handleSelectAddress(item.id)} key={index} style={{ textOverflow: "ellipsis", whiteSpace: "nowrap", overflow: "hidden" }}>
+                    <ListGroup.Item
+                      className="text-dark"
+                      as="li"
+                      onClick={() => handleSelectAddress(item.id)}
+                      key={index}
+                      style={{
+                        textOverflow: "ellipsis",
+                        whiteSpace: "nowrap",
+                        overflow: "hidden",
+                      }}
+                    >
                       {item.fullAddress}
                     </ListGroup.Item>
                   ))}
@@ -126,49 +156,62 @@ const compareAgency = () => {
               )}
             </div>
             {/* <input type='search' onChange={(e) => onChange(e.target)} placeholder="City and State or ZIP" ></input> */}
-            <Button onClick={gotoAgenciesPages} >Compare Agents  <img src={goAhead} alt="goAhead" /></Button>
+            <Button onClick={gotoAgenciesPages}>
+              Compare Agents <img src={goAhead} alt="goAhead" />
+            </Button>
           </div>
         </div>
 
-
         <div className=" cards-container d-flex justify-content-center ">
           <div className="card text-center " style={{ border: "none" }}>
-            <div className="card-body ">
-              <img src={reviewImage} alt="reviewImage" />
-              <p>
-                {" "}
-                <span>
+            <div className="card-body image-setting ">
+              <div>
+                <img src={reviewImage} alt="reviewImage" />
+              </div>
+              <div>
+                <p>
                   {" "}
-                  Read reviews from <br></br> previous customers{" "}
-                </span>{" "}
-              </p>
-              <p>Lorem ipsum dolor sit amet, cons</p>
+                  <span>
+                    {" "}
+                    Read reviews from <br></br> previous customers{" "}
+                  </span>{" "}
+                </p>
+                <p>Lorem ipsum dolor sit amet, cons</p>
+              </div>
             </div>
           </div>
           <div className="card text-center" style={{ border: "none" }}>
-            <div className="card-body">
-              <img src={locationImage} alt="reviewImage" />
-              <p>
-                {" "}
-                <span>
+            <div className="card-body image-setting">
+              <div>
+                <img src={locationImage} alt="reviewImage" />
+              </div>
+              <div>
+                <p>
                   {" "}
-                  View the agencies <br></br> nearest to you{" "}
-                </span>{" "}
-              </p>
-              <p>Lorem ipsum dolor sit amet, cons</p>
+                  <span>
+                    {" "}
+                    View the agencies <br></br> nearest to you{" "}
+                  </span>{" "}
+                </p>
+                <p>Lorem ipsum dolor sit amet, cons</p>
+              </div>
             </div>
           </div>
           <div className="card text-center" style={{ border: "none" }}>
-            <div className="card-body">
-              <img src={homeImage} alt="reviewImage" />
-              <p>
-                {" "}
-                <span>
+            <div className="card-body image-setting">
+              <div>
+                <img src={homeImage} alt="reviewImage" />
+              </div>
+              <div>
+                <p>
                   {" "}
-                  Compare similar houses <br></br> that they sold{" "}
-                </span>{" "}
-              </p>
-              <p>Lorem ipsum dolor sit amet, cons</p>
+                  <span>
+                    {" "}
+                    Compare similar houses <br></br> that they sold{" "}
+                  </span>{" "}
+                </p>
+                <p>Lorem ipsum dolor sit amet, cons</p>
+              </div>
             </div>
           </div>
         </div>
@@ -307,7 +350,6 @@ const compareAgency = () => {
               <Button onClick={gotoAgenciesPages}>
                 Compare Agents <img src={goAhead} alt="goAhead" />
               </Button>
-
             </div>
           </div>
         </div>
@@ -324,7 +366,6 @@ const compareAgency = () => {
     </>
   );
 };
-
 
 export const getServerSideProps = async ({ locale }) => {
   return {
